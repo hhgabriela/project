d637f735b0f69cd4613c414ba6a37bf4ac2dcab1
Gabriela Zarnescu <zarnescu_gabriela@yahoo.com>
2020-03-19 21:08:15 +0200

    added new class

--- lab1-master/src/loose/oose/fis/documents/Document.java
+++ lab1-master/src/loose/oose/fis/documents/Document.java
@@ -13,7 +13,7 @@ public abstract class Document {
     public String toString() {
         String res = "";
         for (String cuvant : continut) {
-            res += cuvant + " ";
+            res += cuvant + ".";
         }
         return res;
     }
